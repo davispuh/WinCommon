f81d5c961482756bc19992e37143ef2c927a3b1d
Dāvis <davispuh@gmail.com>
2014-05-31 13:40:10 +0300

    Add Errror Facility constants

--- lib/win_common/errors.rb
+++ lib/win_common/errors.rb
@@ -2,6 +2,20 @@ module WinCommon
     module Errors
         class WinCommonError < RuntimeError; end
     end
+
+    module Constants
+        FACILITY_NULL     = 0x00
+        FACILITY_RPC      = 0x01
+        FACILITY_DISPATCH = 0x02
+        FACILITY_STORAGE  = 0x03
+        FACILITY_ITF      = 0x04
+        FACILITY_WIN32    = 0x07
+        FACILITY_WINDOWS  = 0x08
+        FACILITY_SSPI     = 0x09
+        FACILITY_CONTROL  = 0x0A
+        FACILITY_CERT     = 0x0B
+        FACILITY_INTERNET = 0x0C
+    end
 end
 
 require_relative 'errors/hresult'
